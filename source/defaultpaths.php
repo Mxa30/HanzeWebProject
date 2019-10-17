47fa19cbabc7d7b6c1fa168626a4086e2be4790f
Max van der Velde <maxvelde_3010@hotmail.com>
2019-10-17 16:15:28 +0200

    Default paths toegevoegd

---
+++ source/defaultpaths.php
@@ -0,0 +1,11 @@
+<?php
+  ob_start();
+
+  define("SOURCE_PATH", dirname(__FILE__));
+  define("PROJECT_PATH", dirname(SOURCE_PATH));
+  define("APP_PATH", SOURCE_PATH . '/app');
+  define("FILTER_PATH", SOURCE_PATH . '/app/component/filter');
+  define("PAGE_PATH", SOURCE_PATH . '/app/page');
+  define("ASSET_PATH", SOURCE_PATH . '/asset');
+
+ ?>
