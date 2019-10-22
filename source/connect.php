c74756ea2eb2ead5bd8dd37864e66dfbd5be8d90
Max van der Velde <maxvelde_3010@hotmail.com>
2019-10-22 17:55:02 +0200

    Connectie gemaakt met database

--- source/connect.php
+++ source/connect.php
@@ -1,30 +1,18 @@
 <?php
-/*	$dbname = 'wideworldimporters';
-	$dbuser = 'root';
-	$dbpass = 'Fasha2808';
-	$dbhost = '127.0.0.1';
-	$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
-	mysqli_select_db($connect, $dbname) or die("Could not open the database '$dbname'");
-
-*/
-
-$servername = "ssh@78.46.199.112";
-$username = "root";
+// Maak in PHPMyAdmin een gebruiker aan met de gebruiersnaam: roomsServiceAdmin
+// wachtwoord: Strongpassword123
+// servernaam: lokaal
+// en bij rechten, alles onder de tabel "Data" dus SELECT, INSERT, UPDATE, DELETE, FILE
+$servername = "localhost";
+$username = "roomsServiceAdmin";
 $password = "Strongpassword123";
 $dbname = "roomservice";
 
 // Create connection
-$connect = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($connect->connect_error) {
-    die("Connection failed: " . $connect->connect_error);
-    echo "failed to connect";
-}else {
-  echo "connection succesfull";
+if (mysqli_connect_errno($conn)){
+ $message = "Connectie Database mislukt: " . mysqli_connect_error();
+ echo "<script type='text/javascript'>alert('$message');</script>";
 }
-
-
-
-
-
 ?>
