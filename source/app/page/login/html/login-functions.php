d296e8b7d062eab713040730267ecfc99a549e48
GD00RN <55438101+GD00RN@users.noreply.github.com>
2019-10-28 13:23:28 +0100

    login gefixt.

--- source/app/page/login/html/login-functions.php
+++ source/app/page/login/html/login-functions.php
@@ -11,11 +11,23 @@ if($link){
 if(isset($_POST['submit'])){
   $email = $_REQUEST['email'];
   $password = $_REQUEST['password'];
-  $sqlLoginQuery = "insert into Login (email, password) values ('$email', '$password');";
+  $sqlAanmeldquery = "insert into Login (email, password) values ('$email', '$password');";
+  if (mysqli_query($conn, $sqlAanmeldquery)) {
+    // Added successfully
+  }else{
+    echo "Error: " . $sqlAanmeldquery . "<br>" . mysqli_error($conn);
+  }
+echo "dikke prima";
+}
+
+if(isset($_POST['login'])){
+  $email = $_REQUEST['email'];
+  $password = $_REQUEST['password'];
+  $sqlLoginQuery = "SELECT email, password FROM login WHERE email = '$email' AND password = '$password';";
   if (mysqli_query($conn, $sqlLoginQuery)) {
     // Added successfully
   }else{
-    echo "Error: " . $sqlLoginQuery . "<br>" . mysqli_error($conn);
+    echo "Verkeerde login of wachtwoord: " . $sqlLoginQuery . "<br>" . mysqli_error($conn);
   }
 echo "dikke prima";
 }
