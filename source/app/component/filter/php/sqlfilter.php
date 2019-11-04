e74bd554efb6516df059ed30bfcb3640dd1c9207
GD00RN <55438101+GD00RN@users.noreply.github.com>
2019-11-04 15:42:11 +0100

    Filters w.i.p. ik zit vast

--- source/app/component/filter/php/sqlfilter.php
+++ source/app/component/filter/php/sqlfilter.php
@@ -24,137 +24,235 @@ $sqlGetOptionVerdiepingResult = mysqli_query($conn, $sqlGetOptionVerdiepingQuery
 $sqlGetOptionFaciliteitQuery = "
 select faciliteit
 from faciliteit
+where faciliteit != ' '
 order by faciliteit";
 
 $sqlGetOptionFaciliteitResult = mysqli_query($conn, $sqlGetOptionFaciliteitQuery);
 
 // Functie om querys te maken die worden verstuurd naar de database om gefilterde kamers te laten zien.
-function filterFunc($conn,$doorenCheck,$olstCheck,$aCheck,$bCheck,$cCheck,$dCheck,$eCheck,$Check1,$Check2,$Check3,$digiCheck,$stopCheck,$whiteCheck){
+function filterFunc($conn,$doorenCheck,$olstCheck,$aCheck,$bCheck,$cCheck,$dCheck,$eCheck,$Check0,$Check1,$Check2,$Check3,$Check4,$digiCheck,$stopCheck,$whiteCheck){
   // Als DoorenVeste is gecheckt, maak dan de deelquery, maak anders een lege string
   if ($doorenCheck) {
-    $doorenQuery = "
-    gebouw = 'Van DoorenVeste'";
-    $indexWaardeGebouw=0;
+    $doorenQuery = "  (gebouw = 'Van DoorenVeste'";
   }else {
     $doorenQuery = "";
   }
 
   // Als OlstToren is gecheckt, maak dan de deelquery, maak anders een lege string
-  if ($olstCheck) {
-    $olstQuery = "
-    gebouw = 'Van OlstToren'";
-    $indexWaardeGebouw=1;
+  if ($doorenCheck && $olstCheck) {
+    $olstQuery = " or gebouw = 'Van OlstToren'";
+  }elseif($olstCheck) {
+    $olstQuery = " (gebouw = 'Van OlstToren'";
   }else {
     $olstQuery = "";
   }
+
   // Maak array met daarin de waardes van de deelquerys
     $gebouwArray = array($doorenQuery,$olstQuery);
 
   // Als Vleugel a is gecheckt, maak dan de deelquery, maak anders een lege string
   if ($aCheck) {
-    $aQuery = "
-    vleugel = 'A'";
-    $indexWaardeVleugel=0;
+    $aQuery = " (vleugel = 'A'";
   }else {
     $aQuery = "";
   }
 
   // Als Vleugel b is gecheckt, maak dan de deelquery, maak anders een lege string
   if ($aCheck && $bCheck) {
-    $bQuery = "
-    and vleugel = 'B'";
-    $indexWaardeVleugel=1;
+    $bQuery = " or vleugel = 'B'";
   } elseif($bCheck) {
-    $bQuery = "
-    vleugel = 'B'";
-    $indexWaardeVleugel=1;
+    $bQuery = " (vleugel = 'B'";
   } else {
     $bQuery = "";
   }
 
   // Als Vleugel c is gecheckt, maak dan de deelquery, maak anders een lege string
   if ($aCheck && $bCheck && $cCheck) {
-    $cQuery = "
-    and vleugel = 'C'";
-    $indexWaardeVleugel=2;
+    $cQuery = " or vleugel = 'C'";
   }elseif($aCheck && $cCheck) {
-    $cQuery = "
-    and vleugel = 'C'";
-    $indexWaardeVleugel=2;
+    $cQuery = " or vleugel = 'C'";
   }elseif($bCheck && $cCheck) {
-    $cQuery = "
-    and vleugel = 'C'";
-    $indexWaardeVleugel=2;
+    $cQuery = " or vleugel = 'C'";
   }elseif($cCheck){
-    $cQuery = "
-    vleugel = 'C'";
+    $cQuery = " (vleugel = 'C'";
   }else {
     $cQuery = "";
   }
 
   // Als Vleugel d is gecheckt, maak dan de deelquery, maak anders een lege string
   if ($aCheck && $bCheck && $cCheck && $dCheck) {
-    $dQuery = "
-    and vleugel = 'D'";
-    $indexWaardeVleugel=3;
+    $dQuery = " or vleugel = 'D'";
   }elseif($aCheck && $dCheck) {
-    $dQuery = "
-    and vleugel = 'D'";
-    $indexWaardeVleugel=2;
+    $dQuery = " or vleugel = 'D'";
   }elseif($bCheck && $dCheck) {
-    $dQuery = "
-    and vleugel = 'D'";
-    $indexWaardeVleugel=2;
+    $dQuery = " or vleugel = 'D'";
   }elseif($cCheck && $dCheck) {
-    $dQuery = "
-    and vleugel = 'D'";
-    $indexWaardeVleugel=2;
+    $dQuery = " or vleugel = 'D'";
   }elseif($dCheck){
-    $dQuery = "
-    vleugel = 'D'";
+    $dQuery = " (vleugel = 'D'";
   }else {
     $dQuery = "";
   }
 
   // Als Vleugel e is gecheckt, maak dan de deelquery, maak anders een lege string
   if ($aCheck && $bCheck && $cCheck && $dCheck && $eCheck) {
-    $eQuery = "
-    and vleugel = 'E'";
-    $indexWaardeVleugel=3;
+    $eQuery = " or vleugel = 'E'";
   }elseif($aCheck && $eCheck) {
-    $eQuery = "
-    and vleugel = 'E'";
-    $indexWaardeVleugel=2;
+    $eQuery = " or vleugel = 'E'";
   }elseif($bCheck && $eCheck) {
-    $eQuery = "
-    and vleugel = 'E'";
-    $indexWaardeVleugel=2;
+    $eQuery = " or vleugel = 'E'";
   }elseif($cCheck && $eCheck) {
-    $eQuery = "
-    and vleugel = 'E'";
-    $indexWaardeVleugel=2;
+    $eQuery = " or vleugel = 'E'";
   }elseif($dCheck && $eCheck) {
-    $eQuery = "
-    and vleugel = 'E'";
-    $indexWaardeVleugel=2;
+    $eQuery = " or vleugel = 'E'";
   }elseif($eCheck){
-    $eQuery = "
-    vleugel = 'E'";
+    $eQuery = " (vleugel = 'E'";
   }else {
     $eQuery = "";
   }
 
   // Maak array met daarin de waardes van de deelquerys
-  $vleugelArray = array($aQuery,$bQuery,$cQuery,$dQuery,$eQuery,);
-  echo 'where', $aQuery, $bQuery, $cQuery, $dQuery, $eQuery, ';';
+  $vleugelArray = array($aQuery,$bQuery,$cQuery,$dQuery,$eQuery);
+
+  if ($Check0) {
+    $Query0 = " (verdieping = '0'";
+  }else {
+    $Query0 = "";
+  }
+
+  if ($Check0 && $Check1) {
+    $Query1 = " or verdieping = '1'";
+  }elseif ($Check1) {
+    $Query1 = " (verdieping = '1'";
+  }else {
+    $Query1 = "";
+  }
+
+  if ($Check0 && $Check1 && $Check2) {
+    $Query2 = " or verdieping = '2'";
+  }elseif ($Check0 && $Check2) {
+    $Query2 = " or verdieping = '2'";
+  }elseif ($Check1 && $Check2) {
+    $Query2 = " or verdieping = '2'";
+  }elseif ($Check2) {
+    $Query2 = " (verdieping = '2'";
+  }else {
+    $Query2 = "";
+  }
+
+  if ($Check0 && $Check1 && $Check2 && $Check3) {
+    $Query3 = " or verdieping = '3'";
+  }elseif ($Check0 && $Check3) {
+    $Query3 = " or verdieping = '3'";
+  }elseif ($Check1 && $Check3) {
+    $Query3 = " or verdieping = '3'";
+  }elseif ($Check2 && $Check3) {
+    $Query3 = " or verdieping = '3'";
+  }elseif ($Check3) {
+    $Query3 = " (verdieping = '3'";
+  }else {
+    $Query3 = "";
+  }
+
+  if ($Check0 && $Check1 && $Check2 && $Check3 && $Check4) {
+    $Query4 = " or verdieping = '4'";
+  }elseif ($Check0 && $Check4) {
+    $Query4 = " or verdieping = '4'";
+  }elseif ($Check1 && $Check4) {
+    $Query4 = " or verdieping = '4'";
+  }elseif ($Check2 && $Check4) {
+    $Query4 = " or verdieping = '4'";
+  }elseif ($Check3 && $Check4) {
+    $Query4 = " or verdieping = '4'";
+  }elseif ($Check4) {
+    $Query4 = " (verdieping = '4'";
+  }else {
+    $Query4 = "";
+  }
+
+  // Maak array met daarin de waardes van de deelquerys
+  $verdiepingArray = array($Query0,$Query1,$Query2,$Query3,$Query4);
+
+  if ($digiCheck) {
+    $digiQuery = " (faciliteit = 'digibord'";
+  }else {
+    $digiQuery = "";
+  }
+
+  if ($digiCheck && $stopCheck) {
+    $stopQuery = " or faciliteit = 'stopcontacten'";
+  }elseif ($stopCheck) {
+    $stopQuery = " (faciliteit = 'stopcontacten'";
+  }else {
+    $stopQuery = "";
+  }
+
+  if ($digiCheck && $stopCheck && $whiteCheck) {
+    $whiteQuery = " or faciliteit = 'whiteboard'";
+  }elseif ($digiCheck && $whiteCheck) {
+    $whiteQuery = " or faciliteit = 'whiteboard'";
+  }elseif ($stopCheck && $whiteCheck) {
+    $whiteQuery = " or faciliteit = 'whiteboard'";
+  }elseif ($whiteCheck) {
+    $whiteQuery = " (faciliteit = 'whiteboard'";
+  }else {
+    $whiteQuery = "";
+  }
+
+  // Maak array met daarin de waardes van de deelquerys
+  $faciliteitArray = array($digiQuery,$stopQuery,$whiteQuery);
+
+  // Als niet alle gegevens zijn ingevuld geef dan een foutcode.
+  // if ($doorenQuery == $olstQuery){
+  //   echo '<script language="javascript">';
+  //   echo 'alert("Vul al de gegevens in")';
+  //   echo '</script>';
+  //   return;
+  // } elseif ($aQuery == $bQuery && $bQuery == $cQuery && $cQuery == $dQuery && $dQuery == $eQuery){
+  //   echo '<script language="javascript">';
+  //   echo 'alert("Vul al de gegevens in")';
+  //   echo '</script>';
+  //   return;
+  // } elseif ($Query0 == $Query1 && $Query1 == $Query2 && $Query2 == $Query3 && $Query3 == $Query4){
+  //   echo '<script language="javascript">';
+  //   echo 'alert("Vul al de gegevens in")';
+  //   echo '</script>';
+  //   return;
+  // } elseif ($digiQuery == $stopQuery && $stopQuery == $whiteQuery){
+  //   echo '<script language="javascript">';
+  //   echo 'alert("Vul al de gegevens in")';
+  //   echo '</script>';
+  //   return;
+  // }
+
+  // Gebruik dit om de where statement te laten zien na alle gegevens te hebben ingevuld.
+  echo 'where';
+  echo $doorenQuery,$olstQuery, ')';
+
+  echo $aQuery,$bQuery,$cQuery,$dQuery,$eQuery, ')';
+  if ($aQuery != $bQuery && $bQuery != $cQuery && $cQuery != $dQuery && $dQuery != $eQuery){
+    echo ' and';
+  }
+  echo $Query0,$Query1,$Query2,$Query3,$Query4, ')';
+  if ($Query0 != $Query1 && $Query1 != $Query2 && $Query2 != $Query3 && $Query3 != $Query4){
+    echo ' and';
+  }
+  echo $digiQuery,$stopQuery,$whiteQuery, ')';
+  if ($digiQuery != $stopQuery && $stopQuery != $whiteQuery){
+    echo ' and';
+  }
+  echo ';';
   // Maak array met daarin de gemaakte arrays
-  $deelQueryFilter = array($gebouwArray,$vleugelArray);
+  $deelQueryFilter = array($gebouwArray,$vleugelArray,$verdiepingArray,$faciliteitArray);
 
   // Gebuik dit om te checken hoe de array er uit ziet. breid alles hier boven uit zodat alles in de array komt.
-  echo "<pre>";
-  print_r ($deelQueryFilter);
-  echo "</pre>";
+  // echo "<pre>";
+  // print_r ($deelQueryFilter);
+  // echo "</pre>";
+
+
 }
+
 
 // Check of er een verzoek wordt gedaan om iets uit te voeren: toepassen knop
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -199,6 +297,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $eCheck = true;
       }
 
+      if (!isset($_POST['0'])) {
+        $Check0 = false;
+      }else {
+        $Check0 = true;
+      }
       if (!isset($_POST['1'])) {
         $Check1 = false;
       }else {
@@ -214,24 +317,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       }else {
         $Check3 = true;
       }
+      if (!isset($_POST['4'])) {
+        $Check4 = false;
+      }else {
+        $Check4 = true;
+      }
 
-      if (!isset($_POST['Digibord'])) {
+      if (!isset($_POST['digibord'])) {
         $digiCheck = false;
       }else {
         $digiCheck = true;
       }
-      if (!isset($_POST['Stopcontacten'])) {
+      if (!isset($_POST['stopcontacten'])) {
         $stopCheck = false;
       }else {
         $stopCheck = true;
       }
-      if (!isset($_POST['Whiteboard'])) {
+      if (!isset($_POST['whiteboard'])) {
         $whiteCheck = false;
       }else {
         $whiteCheck = true;
       }
       // Parse alle waardes naar de functie filterFunc
-      filterFunc($conn,$doorenCheck,$olstCheck,$aCheck,$bCheck,$cCheck,$dCheck,$eCheck,$Check1,$Check2,$Check3,$digiCheck,$stopCheck,$whiteCheck);
+      filterFunc($conn,$doorenCheck,$olstCheck,$aCheck,$bCheck,$cCheck,$dCheck,$eCheck,$Check0,$Check1,$Check2,$Check3,$Check4,$digiCheck,$stopCheck,$whiteCheck);
   }
 
 //if ($deimtenCheck = true){
